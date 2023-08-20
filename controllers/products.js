93b3812ff2fc26069d50fabac54c23c74c885540
Thinh Truong Cong <truongcongthinh.contact@gmail.com>
2023-08-20 14:33:23 +0700

    Add get-product-by-id feature

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 const { Op } = require("sequelize");
 const asyncHandler = require("../middlewares/asyncHandler");
+const ErrorResponse = require("../utils/errorResponse");
 module.exports.getProducts = asyncHandler(async (req, res, next) => {
   const { page = 1, limit = 5, q = "", sort = "ProductID:asc" } = req.query;
 
@@ -34,4 +35,15 @@ module.exports.getProducts = asyncHandler(async (req, res, next) => {
     totalPages,
     totalItems: count,
   });
+});
+
+module.exports.getProductByID = asyncHandler(async (req, res, next) => {
+  const productId = req.params.id;
+  const product = await Product.findByPk(productId);
+  console.log(product);
+  if (!product) {
+    return next(new ErrorResponse("Product not found", 404));
+  }
+
+  res.json({ statusCode: res.statusCode, product });
 });
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts } = require("../controllers/products");
+const { getProducts, getProductByID } = require("../controllers/products");
 
 router.get("/products", getProducts);
+router.get("/products/:id", getProductByID);
 
 module.exports = router;
